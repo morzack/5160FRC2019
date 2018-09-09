b1db30778a1855d3a7e8c5ce9b8e36fb0121cd24
John Kesler <John_Kesler@caryacademy.org>
2018-09-09 16:57:03 -0400

    updated codebase with new drivetrain, motor controllers, and auto test

--- src/Flavor Blasted Xtra Cheddar/robot.py
+++ src/Flavor Blasted Xtra Cheddar/robot.py
@@ -20,8 +20,14 @@ class Robot(wpilib.IterativeRobot):
         self.motorLeftBack = wpilib.PWMTalonSRX(Robot.backLeftPort)
         self.motorRightFront =  wpilib.PWMTalonSRX(Robot.frontRightPort)
         self.motorRightBack = wpilib.PWMTalonSRX(Robot.backRightPort)
+        # invert motors
+        self.motorLeftFront.setInverted(True)
+        self.motorLeftBack.setInverted(True)
+        # make motor groups
+        self.leftMotors = wpilib.SpeedControllerGroup(self.motorLeftBack, self.motorLeftFront)
+        self.rightMotors = wpilib.SpeedControllerGroup(self.motorRightBack, self.motorRightFront)
         # create a drivetrain ovject to access motors easier
-        self.drivetrain = wpilib.drive.MecanumDrive(self.motorLeftFront, self.motorLeftBack, self.motorRightFront, self.motorRightBack)
+        self.drivetrain = wpilib.drive.DifferentialDrive(self.leftMotors, self.rightMotors)
         # set up a timer to allow for cheap drive by time auto
         self.timer = wpilib.Timer()
         # initialize OI systems for the robot 
@@ -34,22 +40,25 @@ class Robot(wpilib.IterativeRobot):
         self.timer.start()
 
     def autonomousPeriodic(self):
-        # just dont do anything for autonomous
-        # iirc this method is called repeatedly
-        if self.timer.accumulatedTime < 2.0:
-            self.drivetrain.driveCartesian(0.5, 0, 0)
+        # this method is called repeatedly
+        if self.timer.get() < 2.0:
+            self.drivetrain.tankDrive(-0.5, -0.5)
         else:
-            self.drivetrain.driveCartesian(0, 0, 0)  # Stop robot
+            self.drivetrain.tankDrive(0, 0)  # Stop robot
+        self.motorLeftBack.set
+    
+    def teleopInit(self):
+        # teleop period initialization
+        pass
 
     def teleopPeriodic(self):
         # teleop method, called repeatedly
         # make OI do special input things
         self.OI.handleInput()
         # move the mecanum DT w/ OI modifiers
-        self.drivetrain.driveCartesian(self.OI.handleNumber(self.OI.joystick0.getY()),
-                                        self.OI.handleNumber(self.OI.joystick0.getX()), 
-                                        self.OI.joystick0.getTwist()+self.OI.joystick0.getAxis(4))
-
+        self.drivetrain.tankDrive(self.OI.handleNumber(self.OI.joystick0.getY(wpilib.XboxController.Hand.kLeft)),
+                                        self.OI.handleNumber(self.OI.joystick0.getY(wpilib.XboxController.Hand.kRight)))
+    
 # this is NEEDED because threads are a thing
 # you dont want like 5 robot code instnaces, right?
 if __name__ == "__main__":
