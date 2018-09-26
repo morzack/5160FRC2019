f32fa1562459cb60b58464d0bc694b36dc172430
John Kesler <John_Kesler@caryacademy.org>
2018-09-26 09:23:45 -0400

    tuned center dodging auto

--- FBXC Rewrite/commands/centerDodge.py
+++ FBXC Rewrite/commands/centerDodge.py
@@ -2,10 +2,14 @@ from wpilib.command import CommandGroup
 
 from commands import driveForwards, turnRobot
 
+import math
+
 class CenterDodge(CommandGroup):
     def __init__(self, robot):
         super().__init__()
         # go forwards, turn, go forwards again
-        self.addSequential(driveForwards.DriveForward(robot, 60))
-        self.addSequential(turnRobot.TurnRobot(robot, 45))
-        self.addSequential(driveForwards.DriveForward(robot, 84.85))
+        initialDistance = 80
+        turnAngle = 5
+        self.addSequential(driveForwards.DriveForward(robot, initialDistance))
+        self.addSequential(turnRobot.TurnRobot(robot, turnAngle))
+        self.addSequential(driveForwards.DriveForward(robot, (144-initialDistance)/math.cos(math.radians(turnAngle))))
