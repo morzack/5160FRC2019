a383f3c570ccbd327688ed321f4c363f33bd4a66
John Kesler <John_Kesler@caryacademy.org>
2018-09-26 09:25:03 -0400

    updated filestructure

--- FBXC/robot.py
+++ FBXC/robot.py
@@ -1,87 +1,105 @@
 #!/usr/bin/env python3
 
 import wpilib
-import wpilib.drive
-import ctre
+from wpilib.command import Scheduler
 
-from commandbased import CommandBasedRobot
-from commands import autonomousProgram
+from oi import OI
 
 import logging
 
-import OI
-import robotmap
-from systems import subsystems
+from subsystems import drivetrain, intake, lift
 
-class Robot(CommandBasedRobot):
+from commands import passAutoLineShoot, centerDodge, passAutoLine
+
+class Robot(wpilib.IterativeRobot):
     def robotInit(self):
-        # setup camera server
-        wpilib.CameraServer.launch()
-        
-        # initialize subsystems
-        subsystems.init()
-        
-        # misc initializations
-        # set up a timer to allow for cheap drive by time auto
-        self.timer = wpilib.Timer()
-        
-        # initialize OI systems for the robot 
-        self.OI = OI.OI()
+        self.drivetrain = drivetrain.Drivetrain(self)
+        self.intake = intake.Intake(self)
+        self.lift = lift.Lift(self)
 
-        #set up a logger
+        self.oi = OI(self)
+
         self.logger = logging.getLogger("robot")
 
-        # auto program
-        # self.autoProgram = autonomousProgram.AutoProgram()
+        self.autonomousCommand = None
+
+    def chooseAuto(self, station, field):
+        closest = field[0]
+        if station == 2:
+            self.autonomousCommand = centerDodge.CenterDodge(self)
+            return
+        if closest=="L":
+            if station == 1:
+                self.autonomousCommand = passAutoLineShoot.PassAutoLineShoot(self)
+            if station == 3:
+                self.autonomousCommand = passAutoLine.PassAutoLine(self)
+        elif closest=="R":
+            if station == 1:
+                self.autonomousCommand = passAutoLine.PassAutoLine(self)
+            if station == 3:
+                self.autonomousCommand = passAutoLineShoot.PassAutoLineShoot(self)
+        else:
+            self.autonomousCommand = passAutoLine.PassAutoLine(self)
 
     def autonomousInit(self):
-        # this runs before the autonomous
-        #get the alliance the robot is on
-        #returns an Alliance, which is an int enum
-        #0 = Red
-        #1 = Blue
-        #2 = Invalid
-        self.alliance = wpilib.DriverStation.getInstance().getAlliance()
+        # get field data for auto
+        alliance = wpilib.DriverStation.getInstance().getAlliance()
 
-        #get the position the robot takes at the driver station wall
-        #returns an int - 1, 2, or 3 depending on the location of the robot
-        self.station = wpilib.DriverStation.getInstance().getLocation()
+        # get the position the robot takes at the driver station wall
+        # returns an int - 1, 2, or 3 depending on the location of the robot
+        station = wpilib.DriverStation.getInstance().getLocation()
 
-        #Get game specific message to determine order of plates on switches and scale
-        #Make sure this runs at the END of autoInit so that the data can arrive from the FMS
-        #TODO: TEST THIS - MAKE SURE THAT GAME DATA CONSISTENTLY GETS READ PROPERLY
-        self.gameData = wpilib.DriverStation.getInstance().getGameSpecificMessage()
+        # Get game specific message to determine order of plates on switches and scale
+        # Make sure this runs at the END of autoInit so that the data can arrive from the FMS
+        gameData = wpilib.DriverStation.getInstance().getGameSpecificMessage()
 
-        #log if not read correctly
-        #set self.gameData so the robot will know to cross the driveline insead of going to switch
-        #that way, the robot will always pass the driveline even if the data isn't read correctly.
-        if len(self.gameData) < 3:
+        # log if not read correctly
+        # set self.gameData so the robot will know to cross the driveline insead of going to switch
+        # that way, the robot will always pass the driveline even if the data isn't read correctly.
+        if len(gameData) < 3:
             self.logger.info("Game data not read correctly!!!")
-            self.gameData = "CCC"
+            gameData = "CCC"
         else:
-            #sends received data
-            self.logger.info("Game data read as: {}".format(self.gameData))
-        
-        # run autonomous
-        # self.autoProgram.addData(self.gameData, self.station)
-        # self.autoProgram.start()
+            # sends received data
+            self.logger.info("Game data read as: {}".format(gameData))
 
-    def teleopInit(self):
-        # teleop period initialization
+        self.chooseAuto(station, gameData)
+        self.autonomousCommand.start()
         pass
 
-    def teleopPeriodic(self):
-        # teleop method, called repeatedly
-        # make OI do special input things
-        self.OI.handleInput()
+    def autonomousPeriodic(self):
+        """This function is called periodically during autonomous"""
+        Scheduler.getInstance().run()
+        self.log()
 
-        # move the mecanum DT w/ OI modifiers
-        subsystems.dt.handleDriving(self.OI, 0)
-        
-        # do stuff with intake
-        subsystems.inT.handleIntake(self.OI, 2)
-    
-# this is NEEDED because threads are a thing
-# you dont want like 5 robot code instnaces, right?
+    def teleopInit(self):
+        """This function is called at the beginning of operator control."""
+        #This makes sure that the autonomous stops running when
+        #teleop starts running. If you want the autonomous to
+        #continue until interrupted by another command, remove
+        #this line or comment it out.
+        if self.autonomousCommand is not None:
+            self.autonomousCommand.cancel()
+        self.drivetrain.encoder.reset()
+
+    def teleopPeriodic(self):
+        """This function is called periodically during operator control."""
+        Scheduler.getInstance().run()
+        self.log()
+
+    def testPeriodic(self):
+        """This function is called periodically during test mode."""
+        # wpilib.LiveWindow.run()
+
+    def disabledInit(self):
+        pass
+
+    def disabledPeriodic(self):
+        """This function is called periodically while disabled."""
+        self.log()
+
+    def log(self):
+        pass
+
 if __name__ == "__main__":
     wpilib.run(Robot)
